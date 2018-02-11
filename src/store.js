88e9db3f56f4466bbbbe36836a766594d5ccab87
Rob Huehn <rhuehn@gmail.com>
2018-02-11 12:51:50 -0800

    add redux-thunk and redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,13 +1,20 @@
-import {createStore} from "redux"
+import {createStore, applyMiddleware, compose} from "redux"
 import reducer from "./reducer"
+import thunk from 'redux-thunk'
 
-const initialState = {
-    ship: {
-        name: '',
-        searching: false,
-        error: ''
-    }
-}
+const
+    initialState = {
+        ship: {
+            name: '',
+            searching: false,
+            error: ''
+        }
+    },
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-export const store = createStore(reducer, initialState)
+export const store = createStore(
+    reducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk))
+)
 
