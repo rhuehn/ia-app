973d2e4d7d91d2b6454a9d0c543d38edc5c7b1b6
Rob Huehn <rhuehn@gmail.com>
2018-02-11 20:01:52 -0800

    enable marker

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import {getGoogleKey, Map, Marker, GoogleApiComponent} from '../external'
+import {connect} from 'react-redux'
 
-class _MapContainer extends React.Component {
+const
+    mapState = ({marker}) => ({marker})
+
+const _MapContainer = connect(mapState)(class extends React.Component {
     render () {
         return (
             <div>
                 <Map google={this.props.google}>
-                    {/*{this.props.markers.map(marker =>*/}
-                        {/*<Marker*/}
-                            {/*key={marker.get('title')}*/}
-                            {/*title={marker.get('title')}*/}
-                            {/*description={marker.get('description')}*/}
-                            {/*properties={marker.get('properties')}*/}
-                            {/*position={marker.get('position')}*/}
-                            {/*mapOn={marker.get('mapOn')}*/}
-                            {/*addMarker={this.props.addMarker}*/}
-                            {/*onMarkerClick={this.props.onMarkerClick}/>*/}
-                    {/*)}*/}
+                    {this.props.marker &&
+                        <Marker
+                            {...this.props.marker}
+                            // onMarkerClick={this.props.onMarkerClick}
+                        />
+                    }
                 </Map>
             </div>
         )
     }
-}
+})
 
 export const MapContainer = GoogleApiComponent({
     apiKey: getGoogleKey()
